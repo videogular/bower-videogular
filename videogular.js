c78cd66eee352f282cc2df8ca6bd3cb29230a73d
Raul Jimenez <elecash@gmail.com>
2014-02-27 19:26:34 +0100

    Added support for minification. Added minified version. Fixed controlbar rendering on init. Now all plugin HTML templates are embedded in JS files (easier to deploy with bower).

--- videogular.js
+++ videogular.js
@@ -34,7 +34,7 @@ angular.module("com.2fdevs.videogular", ["ngSanitize"])
 			 * @returns {*}
 			 */
 			if (navigator.userAgent.match(/Firefox/i)) {
-				var style = $event.currentTarget.currentStyle || window.getComputedStyle($event.currentTarget, null);
+				var style = $event.currentTarget.currentStyle || window.getComputedStyle($event.target, null);
 				var borderLeftWidth = parseInt(style['borderLeftWidth'], 10);
 				var borderTopWidth = parseInt(style['borderTopWidth'], 10);
 				var rect = $event.currentTarget.getBoundingClientRect();
@@ -163,7 +163,7 @@ angular.module("com.2fdevs.videogular", ["ngSanitize"])
 					vgUpdateState: "&",
 					vgPlayerReady: "&"
 				},
-				controller: function($scope) {
+				controller: ['$scope', function($scope) {
 					var currentTheme = null;
 					var currentWidth = null;
 					var currentHeight = null;
@@ -583,7 +583,7 @@ angular.module("com.2fdevs.videogular", ["ngSanitize"])
 					};
 
 					$scope.init();
-				},
+				}],
 				link: {
 					pre: function(scope, elem, attr, controller) {
 						controller.videogularElement = elem;
